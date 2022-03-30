1c525a94850e92b43473c616c698a5cff1869c87
Jason Amba <jason.a@fullrama.net>
2022-03-30 10:36:24 +0200

    [Add] Add component folder

---
+++ test/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="flex">
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Contact</h2>
+        </div>
+    );
+}
+
+export default Header;
