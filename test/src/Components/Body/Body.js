c5b1e44a5cfef049a02bb49de22717e5eb9c0617
Jason Amba <jason.a@fullrama.net>
2022-04-07 12:40:38 +0200

    [Add] Add wind graph

--- test/src/Components/Body/Body.js
+++ test/src/Components/Body/Body.js
@@ -4,6 +4,8 @@ import Temperature from '../plotly-graph/temperature';
 import TemperatureComparison from '../plotly-graph/temperature(comparison)';
 import Rainfull from '../plotly-graph/rainfull';
 import RainfullComparison from '../plotly-graph/rainfull(comparison)';
+import Wind from '../plotly-graph/wind';
+import WindComparison from '../plotly-graph/wind(comparison)';
 
 function Body() {
     return(
@@ -50,6 +52,34 @@ function Body() {
             <hr />
             <div className='colorTitle'>
                 <h2>Vent</h2>
+            </div>
+            <div className='cropping'>
+                <div>
+                    <p>Suivi du vent :</p>
+                    <p>Année en cours (dernières 24 heures) :</p>
+                    <Wind/>
+                    <p>Valeur la plus haute :</p>
+                    <p>Valeur la plus basse :</p>
+                </div>
+                <div>
+                    <p>Select date :</p>
+                    <input type="date" name="" id=""/>
+                    <p>Comparaison autres années :</p>
+                    <WindComparison/>
+                </div>
+            </div>
+            <hr />
+            <div className='colorTitle'>
+                <h2>Pression et humidité</h2>
+            </div>
+            <div className='cropping'>
+                <div>
+                    <p>Suivi de la pression :</p>
+                    <p>Année en cours (dernières 24 heures) :</p>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         </div>
     );
